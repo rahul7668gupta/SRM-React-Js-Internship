905d4531f64c61e0f8cadb5deefbc91387698bc7
rahul7668gupta <rahul7668gupta@gmail.com>
2020-07-15 18:55:57 +0530

    Updated Resume Builder Code

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 fetch("data.json").then(response => {
     return response.json();
 }).then(data => {
-    console.log(data.profile);
-    profile(data.profile.basics);
-    details(data.profile.details);
+    console.log(data.profile.basics1);
+    console.log(data.details);
+    profile(data.profile.basics1);
+    details(data.details);
 })
 
 var child_1 = document.querySelector(".child_1");
@@ -28,6 +29,9 @@ var profile = (basic_info) => {
     email.textContent = basic_info.email;
     child_1.append(email);
 
+    var br = document.createElement("br");
+    child_1.append(br);
+
     var mobile = document.createElement("a");
     mobile.textContent = basic_info.mobile;
     child_1.append(mobile);
@@ -39,5 +43,39 @@ var profile = (basic_info) => {
 }
 
 var details = (detail_info) => {
-    console.log(detail_info);
+    // console.log(detail_info);
+    var summaryHeading = document.createElement("h2");
+    summaryHeading.textContent = "Summary";
+    child_2.append(summaryHeading);
+
+    var hr1 = document.createElement("hr");
+    child_2.append(hr1);
+
+    var list = document.createElement("ul");
+    detail_info.summary.map(i => {
+        var listItem = document.createElement("li");
+        listItem.textContent = i;
+        list.append(listItem);
+    })
+
+    child_2.append(list);
+
+    var skillHeading = document.createElement("h2");
+    skillHeading.textContent = "Skills";
+    child_2.append(skillHeading);
+    var skill_hr = document.createElement("hr");
+    child_2.append(skill_hr);
+
+    detail_info.skills.map(i => {
+        var skillType = document.createElement('h3');
+        skillType.textContent = i.type;
+        child_2.append(skillType);
+
+        i.values.map(skill_value => {
+            var skillValue = document.createElement("span");
+            skillValue.textContent = skill_value;
+            skillValue.classList.add("skill");
+            child_2.append(skillValue);
+        })
+    })
 }
